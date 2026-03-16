062c8b84a33ed1758d5b18b4a2a452d2372f5f14
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-03-16 18:08:28 +0530

    till while loop

--- lec3/control_flow.cpp
+++ lec3/control_flow.cpp
@@ -1,16 +1,55 @@
+//Control flow statements in C++
+
 #include <iostream>
 using namespace std;
 
 int main (){
-    int age;
-    cout << "Enter your age: ";
-    cin >> age;
 
-    if (age >= 18){
-        cout << "You are eligible to vote:" << endl;
-    }else {
-        cout << "You are not eligible to vote:" << endl;
+    // int age;
+    // cout << "Enter your age ";
+    // cin >> age;
+    // if (age >= 18) {
+    //     cout << "You are eligible for vote\n";
+    // } else {
+    //     cout << "You are not eligible for vote\n";
+    // }
+     
+    // int n;
+    // cout << "Enter a number: ";
+    // cin >> n;
+    // if (n>=90) {
+    //     cout << "A\n";
+    // } else if (n >=80 && n < 90) {
+    //     cout << "B\n";
+    // } else if (n >= 70 && n < 80) {
+    //     cout << "C\n";
+    // } else if (n >= 60 && n < 70) {
+    //     cout << "D\n";
+    // } else {
+    //     cout << "F\n";
+    // }
+
+    char ch;
+    cout << "Enter a char : ";
+    cin >> ch;
+
+    // if ((ch) >= 'a'  &&  (ch) <= 'z') {
+    //     cout << "Lowercase\n";
+    // } else if ((ch) >= 'A' && (ch) <= 'Z'){
+    //     cout << "Uppercase\n";
+    // } else {
+    //     cout << "Special character\n";
+    // }
+
+    if (ch >=65  && ch <= 90) { //implicit type conversion
+        cout << "Uppercase\n";
+    } else {
+        cout << "Lowercase\n";
     }
-
+     
+    return 0;
 }
+
+
+
 
