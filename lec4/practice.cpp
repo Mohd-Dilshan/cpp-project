6f177633f8d01f9df8a34e87e26edb8d1cf9778b
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-03-25 19:20:46 +0530

    triangle patterns

--- lec4/practice.cpp
+++ lec4/practice.cpp
@@ -2,12 +2,29 @@
 using namespace std;
 
 int main () {
-    int n = 4;
-    for (int i = 0; i<n-1 ; i++) {
-        for (int j =1; j<=i+1; j++ ) {
-            cout << j << " ";
-        }
 
+    // for (int i = 0; i<n-1 ; i++) {
+    //     for (int j =1; j<=i+1; j++ ) {
+    //         cout << j << " ";
+    //     }
+
+    //     cout << endl;
+    // }
+
+    //inverted triangle
+
+    int n = 4;
+    int num = 1;
+
+    for (int i = 0; i<n; i++){
+        //spaces
+        for (int j =0; j<i; j++) {
+            cout << " ";
+        }
+        //NUMS
+        for (int j =0; j<n-i; j++){
+            cout << (i+1);
+        }
         cout << endl;
     }
 
