07d5c04577dac2eb88b144ef8cd1c36ae35c2bd6
Mohd Dilshan <mohd.dilshan.dev@gmail.com>
2026-03-25 17:37:33 +0530

    chars upto A to I

--- lec4/charHw.cpp
+++ lec4/charHw.cpp
@@ -2,16 +2,18 @@
 using namespace std;
 
 int main () {
-    int n = 4;
+    int n = 3;
+
+    char ch = 'A';
+
     for (int i =0; i <n; i++) {
-        char ch = 'A';
         for (int j = 0; j<n; j++) { //inner starts from 0 to n-1
             cout << ch;
             ch = ch +1;
         }
         cout << endl;
     }
-    cout << "End.\n"<<endl;
+    cout << "...End\n"<<endl;
 
     return 0;
 }
